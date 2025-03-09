2e39749aec59388f598f7a5598fc08e4d2ce58bd
ExostFlash <exostflash+pcportable@teamflash.fr>
2025-03-09 11:38:51 +0100

    initial update

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/essentials/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/essentials/footer/footer.component';
+import { HomeComponent } from './components/essentials/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
